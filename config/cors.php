6b241e837f861ffca6a8bd32c80567ba28952192
Bahalq <bahalqadam@gmail.com>
2026-05-31 01:24:23 +0100

    fix(cors): update CORS configuration to allow all origins and disable credentials

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,12 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'https://bahalq.github.io',
-        'http://localhost:5173',
-        'https://parkova-h9b8gtayfwa3cpej.spaincentral-01.azurewebsites.net',
-    ],
-
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
