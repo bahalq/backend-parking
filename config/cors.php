c3b9f55bc425e1c0fff58bc794ab665425dc4da9
Bahalq <bahalqadam@gmail.com>
2026-05-31 01:39:22 +0100

    fix(cors): refine CORS settings in configuration files for improved API access

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,25 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => [
+        'https://bahalq.github.io',
+        'http://localhost:5173',
+        'https://parkova-h9b8gtayfwa3cpej.spaincentral-01.azurewebsites.net',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
